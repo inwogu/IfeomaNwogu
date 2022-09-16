9aa78121022cd64d0e6f067095506ad0191df229
indudwivedi <indudwivedi96@gmail.com>
2022-09-16 01:15:18 -0400

    updates

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
 
+// Code to highlight current menu selection
+document.addEventListener('DOMContentLoaded', function() {
+    const selector = '.nav-item';
+    const elems = Array.from( document.querySelectorAll( selector ) );
+    const navigation = document.querySelector( 'navbar-nav' );
+    
+    function makeActive( evt ) {
+      const target = evt.target;
+      
+      if ( !target || !target.matches( selector ) ) {
+        return;
+      }
+      
+      elems.forEach( elem => elem.classList.remove( 'active' ) );
+        
+        evt.target.classList.add( 'active' );
+    };
+    
+    navigation.addEventListener( 'mousedown', makeActive );
+    
+    } );
+
+
 // Counter to keep track of site visits
 
 var counterContainer = document.querySelector(".website-counter");
