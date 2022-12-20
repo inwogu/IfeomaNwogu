cfc9205262b2374f2ce0edab083c19bb851a993b
indudwivedi <indudwivedi96@gmail.com>
2022-12-20 14:07:25 -0500

    updates

--- script.js
+++ script.js
@@ -19,26 +19,3 @@ document.addEventListener('DOMContentLoaded', function() {
     navigation.addEventListener( 'mousedown', makeActive );
     
     } );
-
-// // Counter to keep track of site visits
-
-// var counterContainer = document.querySelector(".website-counter");
-// var resetButton = document.querySelector("#reset");
-// var visitCount = localStorage.getItem("page_view");
-
-// // Check if page_view entry is present
-// if (visitCount) {
-//   visitCount = Number(visitCount) + 1;
-//   localStorage.setItem("page_view", visitCount);
-// } else {
-//   visitCount = 1;
-//   localStorage.setItem("page_view", 1);
-// }
-// counterContainer.innerHTML = visitCount;
-
-// // Adding onClick event listener
-// resetButton.addEventListener("click", () => {
-//   visitCount = 1;
-//   localStorage.setItem("page_view", 1);
-//   counterContainer.innerHTML = visitCount;
-// });
